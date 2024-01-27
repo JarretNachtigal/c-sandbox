4713820a5ad2da8135b9f7912c3774f3073d7051
Jarret Nachtigal <jarretn1224@gmail.com>
2024-01-27 10:09:14 -0500

    flashcards initial commit

---
+++ flashcards.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+// read from a file containing info on the standard c header files
+// display the definition of the function
+// user inputs the name of the function ex printf()
