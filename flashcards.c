02a70be7d23ab62715c06a116f95ecaf46b74a0a
Jarret Nachtigal <jarretn1224@gmail.com>
2024-01-27 10:11:22 -0500

    flashcards comments and boilerplate

--- flashcards.c
+++ flashcards.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
 // read from a file containing info on the standard c header files
 // display the definition of the function
 // user inputs the name of the function ex printf()
+// the goal will be to continue to imporove this program using the standard library as I go
+
+int main()
+{
+
+    return 0;
+}
